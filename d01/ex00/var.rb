8651a6afc9b66be57bf287ab300818f8da868492
Zain <zmathews@student.wethinkcode.co.za>
2020-08-03 02:31:02 +0200

    duh

---
+++ d01/ex00/var.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+a = 10
+b = "10"
+c = nil
+d = 10.0
+puts("my variables :\n")
+puts("\ta contains: #{a} and is a type: #{a.class}") # it will display integer because fixnum and bignum were essentially merged and show only as integer class
+puts("\tb contains: #{b} and is a type: #{b.class}")
+puts("\tc contains: nil and is a type: #{c.class}")
+puts("\td contains: #{d} and is a type: #{d.class}")
