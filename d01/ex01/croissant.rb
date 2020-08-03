8526c08fe443b733ae9816309a13bb63de173dc9
Zain <zmathews@student.wethinkcode.co.za>
2020-08-03 03:38:37 +0200

    does this count?

--- d01/ex01/croissant.rb
+++ d01/ex01/croissant.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/ruby
 
-puts("jsdmhbfghids")
+def naming_conventions
+	numbers = File.open("numbers.txt")
+	numbers_data = numbers.readlines
+	to_string = numbers_data.join
+	fuck_commas = to_string.delete ","
+	back_to_array = fuck_commas.split("\n")
+	sort_array = back_to_array.sort
+	puts(sort_array)
+	numbers.close
+end
+
+naming_conventions
