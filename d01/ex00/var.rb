6b9fc5589e3d10074b7ae3b74020f8d3df67b2d4
Zain <zmathews@student.wethinkcode.co.za>
2020-08-03 02:38:40 +0200

    oopsie

--- d01/ex00/var.rb
+++ d01/ex00/var.rb
@@ -1,10 +1,15 @@
 #!/usr/bin/ruby
-a = 10
-b = "10"
-c = nil
-d = 10.0
-puts("my variables :\n")
-puts("\ta contains: #{a} and is a type: #{a.class}") # it will display integer because fixnum and bignum were essentially merged and show only as integer class
-puts("\tb contains: #{b} and is a type: #{b.class}")
-puts("\tc contains: nil and is a type: #{c.class}")
-puts("\td contains: #{d} and is a type: #{d.class}")
+
+def print_types
+	a = 10
+	b = "10"
+	c = nil
+	d = 10.0
+	puts("my variables :\n")
+	puts("\ta contains: #{a} and is a type: #{a.class}") # it will display integer because fixnum and bignum were essentially merged and show only as integer class
+	puts("\tb contains: #{b} and is a type: #{b.class}")
+	puts("\tc contains: nil and is a type: #{c.class}")
+	puts("\td contains: #{d} and is a type: #{d.class}")
+end
+
+print_types
